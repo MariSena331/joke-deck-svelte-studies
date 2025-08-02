c7c953ae5eb12b792a42c1d518bb35e42d8e4383
Mariana Sena <35273542+MariSena331@users.noreply.github.com>
2025-08-02 17:48:04 -0300

    chore: implenting filter by category result in the page

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,11 +1,28 @@
 <script lang="ts">
-    export let data: { categories: string[] };
+    import { getFilteredCategory } from "$lib/api/getFilteredCategory.ts";
+
+    type Data = { categories: string[] };
+    type Joke = { id: string; created_at: Date; icon_url: string; updated_at: Date; url: string; value: string } | null;
+
+    const { data } = $props<{ data: Data }>();
     const { categories } = data;
+
+    let filteredCategory = $state<Joke>(null);
+
+    const handleCategoryFilter = async (category: string) => {
+        filteredCategory = await getFilteredCategory(category);
+    }
 </script>
 
 <h1>Welcome to your library project</h1>
 <ul>
     {#each categories as c}
-        <button>{c}</button>
+        <button onclick={() => handleCategoryFilter(c)}>{c}</button>
     {/each}
+    {#if filteredCategory}
+        <div>
+            <p>{filteredCategory.value}</p>
+            <img src={filteredCategory.icon_url} alt="n sei"/>
+        </div>
+    {/if}
 </ul>
